5e98939889605884506c8fd2764411600157c2ca
Woyas <woyasali@gmail.com>
2025-06-22 00:26:02 +0530

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"https://skwoyasali.github.io/Book-Website/",
   plugins: [react(),tailwindcss(),],
 })
